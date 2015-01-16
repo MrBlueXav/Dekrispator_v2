6f63af9ea9c8fdb6d6a01131bfe50e7859cc5bb9
Tomas <tomas.scherrer@gmail.com>
2015-01-16 21:56:12 +0900

    added the .mk files for linux

---
+++ linuxRelease/Synth/subdir.mk
@@ -0,0 +1,78 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Synth/MIDI_application.c \
+../Synth/adsr.c \
+../Synth/audio_conf.c \
+../Synth/blepvco.c \
+../Synth/chorusFD.c \
+../Synth/delay.c \
+../Synth/drifter.c \
+../Synth/main.c \
+../Synth/math_tools.c \
+../Synth/minblep_tables.c \
+../Synth/notesTables.c \
+../Synth/oscillators.c \
+../Synth/phaser.c \
+../Synth/random.c \
+../Synth/resonantFilter.c \
+../Synth/sequencer.c \
+../Synth/sinetable.c \
+../Synth/soundGen.c \
+../Synth/timers.c 
+
+OBJS += \
+./Synth/MIDI_application.o \
+./Synth/adsr.o \
+./Synth/audio_conf.o \
+./Synth/blepvco.o \
+./Synth/chorusFD.o \
+./Synth/delay.o \
+./Synth/drifter.o \
+./Synth/main.o \
+./Synth/math_tools.o \
+./Synth/minblep_tables.o \
+./Synth/notesTables.o \
+./Synth/oscillators.o \
+./Synth/phaser.o \
+./Synth/random.o \
+./Synth/resonantFilter.o \
+./Synth/sequencer.o \
+./Synth/sinetable.o \
+./Synth/soundGen.o \
+./Synth/timers.o 
+
+C_DEPS += \
+./Synth/MIDI_application.d \
+./Synth/adsr.d \
+./Synth/audio_conf.d \
+./Synth/blepvco.d \
+./Synth/chorusFD.d \
+./Synth/delay.d \
+./Synth/drifter.d \
+./Synth/main.d \
+./Synth/math_tools.d \
+./Synth/minblep_tables.d \
+./Synth/notesTables.d \
+./Synth/oscillators.d \
+./Synth/phaser.d \
+./Synth/random.d \
+./Synth/resonantFilter.d \
+./Synth/sequencer.d \
+./Synth/sinetable.d \
+./Synth/soundGen.d \
+./Synth/timers.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Synth/%.o: ../Synth/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -O3 -ffunction-sections -fdata-sections -fsingle-precision-constant -flto -Wunused -Wuninitialized -Wall -Wconversion -Wshadow -Wfloat-equal -D__FPU_USED=1 -DHSE_VALUE=8000000 -DUSE_HAL_DRIVER -DUSE_STM32F4_DISCO -DSTM32F407xx -I"../drivers/STM32_USB_Host_Library/Core/Inc" -I"../drivers/BSP" -I"../drivers/cmsis" -I"../drivers/STM32F4xx_HAL_Driver/Inc" -I"../drivers/USBH_midi_class/Inc" -I"../Synth" -std=c11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ linuxRelease/drivers/BSP/subdir.mk
@@ -0,0 +1,62 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../drivers/BSP/cs43l22.c \
+../drivers/BSP/lis302dl.c \
+../drivers/BSP/lis3dsh.c \
+../drivers/BSP/stm32f4_discovery.c \
+../drivers/BSP/stm32f4_discovery_accelerometer.c \
+../drivers/BSP/stm32f4_discovery_audio.c \
+../drivers/BSP/stm32f4xx_it.c \
+../drivers/BSP/system_stm32f4xx.c \
+../drivers/BSP/usbh_conf.c 
+
+S_UPPER_SRCS += \
+../drivers/BSP/startup_stm32f407xx.S 
+
+OBJS += \
+./drivers/BSP/cs43l22.o \
+./drivers/BSP/lis302dl.o \
+./drivers/BSP/lis3dsh.o \
+./drivers/BSP/startup_stm32f407xx.o \
+./drivers/BSP/stm32f4_discovery.o \
+./drivers/BSP/stm32f4_discovery_accelerometer.o \
+./drivers/BSP/stm32f4_discovery_audio.o \
+./drivers/BSP/stm32f4xx_it.o \
+./drivers/BSP/system_stm32f4xx.o \
+./drivers/BSP/usbh_conf.o 
+
+C_DEPS += \
+./drivers/BSP/cs43l22.d \
+./drivers/BSP/lis302dl.d \
+./drivers/BSP/lis3dsh.d \
+./drivers/BSP/stm32f4_discovery.d \
+./drivers/BSP/stm32f4_discovery_accelerometer.d \
+./drivers/BSP/stm32f4_discovery_audio.d \
+./drivers/BSP/stm32f4xx_it.d \
+./drivers/BSP/system_stm32f4xx.d \
+./drivers/BSP/usbh_conf.d 
+
+S_UPPER_DEPS += \
+./drivers/BSP/startup_stm32f407xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+drivers/BSP/%.o: ../drivers/BSP/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -O3 -ffunction-sections -fdata-sections -fsingle-precision-constant -flto -Wunused -Wuninitialized -Wall -Wconversion -Wshadow -Wfloat-equal -D__FPU_USED=1 -DHSE_VALUE=8000000 -DUSE_HAL_DRIVER -DUSE_STM32F4_DISCO -DSTM32F407xx -I"../drivers/STM32_USB_Host_Library/Core/Inc" -I"../drivers/BSP" -I"../drivers/cmsis" -I"../drivers/STM32F4xx_HAL_Driver/Inc" -I"../drivers/USBH_midi_class/Inc" -I"../Synth" -std=c11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+drivers/BSP/%.o: ../drivers/BSP/%.S
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM GNU Assembler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -O3 -ffunction-sections -fdata-sections -fsingle-precision-constant -flto -Wunused -Wuninitialized -Wall -Wconversion -Wshadow -Wfloat-equal -x assembler-with-cpp -DSTM32F40_41xxx -D__FPU_USED=1 -DHSE_VALUE=8000000 -DSTM32F407VG -DUSE_HAL_DRIVER -DUSE_STM32F4_DISCO -DSTM32F407xx -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
